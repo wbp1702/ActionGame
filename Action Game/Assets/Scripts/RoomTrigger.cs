a88f777ee9bd5f772d8eb000bfe3b35a69cebe48
William Pittenger <106987854+wbp1702@users.noreply.github.com>
2023-11-30 11:39:16 -0600

    Added Player

---
+++ Action Game/Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    public Enemy[] enemies;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.state = Enemy.State.Attacking;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        bool enemiesDead = true;
+        for (int i = 0; i < enemies.Length; i++) if (enemies[i] == null) { enemiesDead = false; break; }
+
+        if (enemiesDead)
+        {
+            Player.Instance.Heal(100);
+        }
+    }
+}
