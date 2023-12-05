f8c2d63530e6326f0af0a5598d5aad4ed2fdcd36
Austin Belloli <belloliaustin19@gmail.com>
2023-12-05 12:14:28 -0600

    Main Menu and Options Menu

--- Action Game/Assets/Scripts/OptionsMenu.cs
+++ Action Game/Assets/Scripts/OptionsMenu.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class OptionsMenu : MonoBehaviour
 {
     public void OnPlayButton()
     {
         SceneManager.LoadScene(2);
     }
+    public void OnMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void OnQuitButton()
     {
         Application.Quit();
-    }
-    public void OnOptionsButton()
-    {
-        SceneManager.LoadScene(1);
     }
 }
